0d04cb5555f33f3092475dd4aa9510eb69fd3ed1
otabek-a <otabekabdurasulov245@gmail.com>
2025-01-17 16:21:15 +0500

    qwert

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 # Get toke from the environment variable
 import os
 from telegram import Bot
-token = os.getenv("TOKEN")
+token = os.environ['TOKEN']
 
-bot = Bot(token)
-CHAT_ID = "86775091"
+
+CHAT_ID = os.environ['chat_id']
+bot = Bot(token,CHAT_ID)
+
+
+video_url = 'https://www.w3schools.com/html/movie.mp4'
+response = bot.send_video(CHAT_ID, video_url, 'hey, it is my own bot!')
+print(response)
+
+
+
+
+audio = bot.send_audio(CHAT_ID, "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+print(audio)
+
+
+
+
+dacument=bot.send_document(CHAT_ID,"https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+print(dacument)
+
+
+message=bot.send_message(CHAT_ID,'SAlom')
+print(message)
+
+
+audio = bot.send_audio(CHAT_ID, "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
+print(audio)
+
+
+
+photo=bot.send_photo(CHAT_ID)
+print(photo)
+
+
+
+
 updates = bot.get_updates()
-update = updates[0]
-text = update.message.text
-print(text)
+update = updates['result'][-1]
+#text = update.message.text
+print(update)
--- telegram/bot.py
+++ telegram/bot.py
@@ -1,10 +1,13 @@
 import requests
 from .user import User
 class Bot:
-    def __init__(self, token):
-        self.token = token
-        self.base_url = f"https://api.telegram.org/bot{token}/"
+    
 
+    def __init__(self, token,chat_id):
+        self.token = token
+        self.chat_id=chat_id
+        self.base_url = f"https://api.telegram.org/bot{token}/"
+   
     def get_info(self):
         """
         This method returns information about the bot
@@ -24,9 +27,14 @@ class Bot:
         response = requests.post(self.base_url + "sendMessage", data=data)
         return response.json()
     
-    def send_photo(self, chat_id, photo):
-        """
-        This method sends a photo to a specific chat 
+    def send_photo(self,chat_id):
+      url = f'https://api.telegram.org/bot{self.token}/sendPhoto'
+      photo_url = 'https://cataas.com/cat'
+      response = requests.post(url, data={'chat_id': chat_id, 'photo': photo_url})
+      return response.json()
+ 
+
+      """This method sends a photo to a specific chat 
 
         Args:
             chat_id: The id of the chat to send the photo to
@@ -34,6 +42,14 @@ class Bot:
         """
         
     def send_document(self, chat_id, document):
+        url=f'{self.base_url}sendDocument'
+        data={
+            'chat_id':chat_id,
+            'document':document
+        }
+        response=requests.post(url,data=data)
+        return response.json()
+
         """
         This method sends a document to a specific chat
 
@@ -42,17 +58,31 @@ class Bot:
             document: The path to the document to send
         """
 
-    def send_audio(self, chat_id, audio):
-        """
-        This method sends an audio to a specific chat
+    def send_audio(self, chat_id, audio_url):
+        url = f'{self.base_url}sendAudio'
+        data = {'chat_id': chat_id, 'audio': audio_url}
+        response = requests.post(url, data=data)
+        return response.json()
+
+
+    """
+            This method sends an audio to a specific chat
 
         Args:
             chat_id: The id of the chat to send the audio to
             audio: The path to the audio to send
         """
 
-    def send_video(self, chat_id, video):
-        """
+    def send_video(self, chat_id, video, caption):
+     url = f'{self.base_url}sendVideo'  # To'g'ri URL
+     data = {
+        'chat_id': chat_id,
+        'video': video,
+        'caption': caption
+     }
+     response = requests.post(url, data=data)  # To'g'ri nomlash
+     return response.json()
+     """
         This method sends a video to a specific chat
 
         Args:
@@ -60,7 +90,12 @@ class Bot:
             video: The path to the video to send
         """
 
-    def send_voice(self, chat_id, voice):
+    def send_voice(self, chat_id, audio_url):
+        url = f'{self.base_url}sendAudio'
+        data = {'chat_id': chat_id, 'audio': audio_url}
+        response = requests.post(url, data=data)
+        return response.json()
+
         """
         This method sends a voice to a specific chat
 
